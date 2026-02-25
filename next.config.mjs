88c5e15afa7081c5e87903aaed41ef6c26516562
balancedlifeharmony-spec <balancedlifeharmony@gmail.com>
2026-02-25 01:38:27 -0500

    next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+import withPWA from "next-pwa";
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default withPWA({
+  ...nextConfig,
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+  },
+});
